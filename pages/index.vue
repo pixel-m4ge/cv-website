919c1687d657b3fdeee21e758b23894ad6b0a2f6
pixel-m4ge <zoecurry@outlook.com>
2024-06-11 20:48:39 +0100

    ✨ new feature: home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,21 @@
 <template>
   <v-card class="rounded-0 h-100">
     <v-tabs v-model="tab" align-tabs="center" bg-color="indigo">
-      <v-tab prepend-icon="mdi-home" value="tab-1">Summary</v-tab>
+      <v-tab class="text-capitalize" prepend-icon="mdi-home" value="tab-1"
+        >Summary</v-tab
+      >
 
-      <v-tab prepend-icon="mdi-briefcase" value="tab-2">Experience</v-tab>
+      <v-tab class="text-capitalize" prepend-icon="mdi-briefcase" value="tab-2"
+        >Experience</v-tab
+      >
 
-      <v-tab prepend-icon="mdi-medal" value="tab-3">Awards</v-tab>
+      <v-tab class="text-capitalize" prepend-icon="mdi-medal" value="tab-3"
+        >Awards</v-tab
+      >
 
-      <v-tab prepend-icon="mdi-school" value="tab-4">Education</v-tab>
+      <v-tab class="text-capitalize" prepend-icon="mdi-school" value="tab-4"
+        >Education</v-tab
+      >
     </v-tabs>
 
     <v-tabs-window v-model="tab">
@@ -19,9 +27,44 @@
               <v-card
                 variant="flat"
                 class="bg-indigo-lighten-5"
-                title="Summary"
+                title="Zoe Curry"
               >
-                <v-card-text>...</v-card-text>
+                <template v-slot:subtitle>
+                  <span class="text-body-1">Software Developer</span>
+                </template>
+                <v-card-text>
+                  I have 4+ years of full-stack development experience,
+                  specialising in front-end web development. My dissertation
+                  focused on web accessibility, underlining my dedication to
+                  inclusive web design. I placed third in a competitive UK
+                  coding tournament, demonstrating my proficiency in secure
+                  coding practices. I utilise my creative background to inform
+                  and enhance my development work.
+                </v-card-text>
+                <v-card-text class="pt-0">
+                  <div class="bg-indigo-lighten-4" style="border-radius: 4px">
+                    <div class="pa-4">
+                      <div>Projects</div>
+                      <v-chip
+                        variant="flat"
+                        color="grey-darken-4"
+                        label
+                        class="mr-2 mt-2"
+                        append-icon="mdi-open-in-new"
+                      >
+                        <a
+                          class="text-white"
+                          href="https://github.com/pixel-m4ge"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          aria-label="Go to my CV Website repository"
+                        >
+                          CV Website
+                        </a>
+                      </v-chip>
+                    </div>
+                  </div>
+                </v-card-text>
               </v-card>
             </v-col>
             <v-col sm="2" cols="0"></v-col>
